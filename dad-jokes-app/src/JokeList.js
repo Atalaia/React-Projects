bc63ade5eddd7b7d531e6475dd26820b7c412e3e
Maria Soage <maria.soage01@gmail.com>
2020-10-07 15:26:59 +0200

    add upvoting and downvoting jokes functionality

--- dad-jokes-app/src/JokeList.js
+++ dad-jokes-app/src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import "./JokeList.css";
+import Joke from './Joke';
+import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
+import "./JokeList.css";
 
 class JokeList extends Component {
     static defaultProps = {
@@ -10,6 +12,7 @@ class JokeList extends Component {
         super(props);
         this.state = { jokes: [] };
     }
+
     async componentDidMount() {
         // Load jokes
         let jokes = [];
@@ -17,10 +20,19 @@ class JokeList extends Component {
             let res = await axios.get("https://icanhazdadjoke.com/", {
                 headers: { Accept: "application/json" }
             });
-            jokes.push(res.data.joke);
+            jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
         }
         this.setState({ jokes: jokes });
     }
+
+    handleVote(id, delta) {
+        this.setState(st => ({
+            jokes: st.jokes.map(j =>
+                j.id === id ? { ...j, votes: j.votes + delta } : j
+            )
+        }));
+    }
+
     render() {
         return (
             <div className="JokeList">
@@ -34,7 +46,13 @@ class JokeList extends Component {
 
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(j => (
-                        <div>{j}</div>
+                        <Joke
+                            key={j.id}
+                            votes={j.votes}
+                            text={j.text}
+                            upvote={() => this.handleVote(j.id, 1)}
+                            downvote={() => this.handleVote(j.id, -1)}
+                        />
                     ))}
                 </div>
             </div>
