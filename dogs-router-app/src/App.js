e7b18ce747a6f307607cc36e150f5ee8e84f6c12
Maria Soage <maria.soage01@gmail.com>
2020-10-19 16:08:17 +0200

    refactoring and extracting routes to Routes Component

--- dogs-router-app/src/App.js
+++ dogs-router-app/src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import DogList from "./DogList";
-import DogDetails from "./DogDetails";
 import Navbar from "./Navbar";
+import Routes from "./Routes";
 import whiskey from "./images/whiskey.jpg";
 import tubby from "./images/tubby.jpg";
 import hazel from "./images/hazel.jpg";
-import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -44,20 +42,12 @@ class App extends Component {
     ]
   }
   render() {
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog} />;
-    };
     return (
       <div className="App">
         <Navbar dogs={this.props.dogs} />
-        <Switch>
-          <Route exact path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
-          <Route exact path="/dogs/:name" render={getDog} />
-        </Switch>
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
+        </div>
       </div>
     );
   }
