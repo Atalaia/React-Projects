4a01aa0b0e02e86bcc6e6b162697159a6351ecb4
Maria Soage <maria.soage01@gmail.com>
2020-10-26 19:53:29 +0100

    Add React Router and add routes on App Component

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 import './App.css';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routerProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routerProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
