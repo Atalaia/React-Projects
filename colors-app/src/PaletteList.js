61a3c3fdc8a8878f423c21990f08cb7f78dad937
Maria Soage <maria.soage01@gmail.com>
2020-10-27 20:08:52 +0100

    Add MiniPalette Component and material ui styles

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 import './PaletteList.css';
 
 class PaletteList extends Component {
@@ -9,9 +10,7 @@ class PaletteList extends Component {
             <div className="PaletteList">
                 <h1>React Colors</h1>
                 {palettes.map(palette => (
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
+                    <MiniPalette {...palette} />
                 ))}
             </div>
         );
