c91ec1a381eadb30577465b6dac72cefccec92f8
Maria Soage <maria.soage01@gmail.com>
2020-10-31 18:31:31 +0100

    Add Navbar and Footer to SingleColorPalette

--- colors-app/src/Navbar.js
+++ colors-app/src/Navbar.js
@@ -23,25 +23,27 @@ class Navbar extends Component {
         this.setState({ open: false });
     }
     render() {
-        const { level, changeLevel } = this.props;
+        const { level, changeLevel, showingAllColors } = this.props;
         const { format, open } = this.state;
         return (
             <header className="Navbar">
                 <div className="logo">
                     <Link to="/">reactcolorpicker</Link>
                 </div>
-                <div className="slider-container">
-                    <span>Level: {level}</span>
-                    <div className="slider">
-                        <Slider
-                            defaultValue={level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={changeLevel}
-                        />
+                {showingAllColors && (
+                    <div className="slider-container">
+                        <span>Level: {level}</span>
+                        <div className="slider">
+                            <Slider
+                                defaultValue={level}
+                                min={100}
+                                max={900}
+                                step={100}
+                                onAfterChange={changeLevel}
+                            />
+                        </div>
                     </div>
-                </div>
+                )}
                 <div className="select-container">
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff</MenuItem>
@@ -56,7 +58,8 @@ class Navbar extends Component {
                     message={
                         <span id="message-id">
                             Format Changed To {format.toUpperCase()}
-                        </span>}
+                        </span>
+                    }
                     ContentProps={{
                         "aria-describedby": "message-id"
                     }}
