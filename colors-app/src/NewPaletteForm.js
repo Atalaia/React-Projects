8fa433347f791f8f3f7b52bba01b9fb24e36dce1
Maria Soage <maria.soage01@gmail.com>
2020-11-13 16:09:05 +0100

    Add randomColor and clearPalette

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -78,13 +78,16 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
+    static defaultProps = {
+        maxColors: 20
+    };
     constructor(props) {
         super(props);
         this.state = {
             open: true,
             currentColor: "teal",
             newColorName: "",
-            colors: [],
+            colors: this.props.palettes[0].colors,
             newPaletteName: ""
         };
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
@@ -92,6 +95,8 @@ class NewPaletteForm extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.removeColor = this.removeColor.bind(this);
+        this.clearColors = this.clearColors.bind(this);
+        this.addRandomColor = this.addRandomColor.bind(this);
     }
 
     componentDidMount() {
@@ -167,10 +172,26 @@ class NewPaletteForm extends Component {
         }));
     };
 
-    render() {
-        const { classes } = this.props;
-        const { open } = this.state;
+    clearColors() {
+        this.setState({
+            colors: []
+        });
+    }
 
+    addRandomColor() {
+        //pick random color from existing palettes
+        const allColors = this.props.palettes.map(p => p.colors).flat();
+        var rand = Math.floor(Math.random() * allColors.length);
+        const randomColor = allColors[rand];
+        this.setState({
+            colors: [...this.state.colors, randomColor]
+        });
+    }
+
+    render() {
+        const { classes, maxColors } = this.props;
+        const { open, colors } = this.state;
+        const paletteIsFull = colors.length >= maxColors;
         return (
             <div className={classes.root} >
                 <CssBaseline />
@@ -238,10 +259,15 @@ class NewPaletteForm extends Component {
                         Design Your Palette
                     </Typography>
                     <div>
-                        <Button variant="contained" color="secondary">
+                        <Button variant="contained" color="secondary" onClick={this.clearColors} >
                             Clear Palette
                         </Button>
-                        <Button variant="contained" color="primary">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            disabled={paletteIsFull}
+                            onClick={this.addRandomColor}
+                        >
                             Random Color
                         </Button>
                     </div>
@@ -269,9 +295,14 @@ class NewPaletteForm extends Component {
                             variant="contained"
                             type="submit"
                             color="primary"
-                            style={{ backgroundColor: this.state.currentColor }}
+                            disabled={paletteIsFull}
+                            style={{
+                                backgroundColor: paletteIsFull
+                                    ? "grey"
+                                    : this.state.currentColor
+                            }}
                         >
-                            Add Color
+                            {paletteIsFull ? "Palette Full" : "Add Color"}
                         </Button>
                     </ValidatorForm>
                 </Drawer>
@@ -282,10 +313,10 @@ class NewPaletteForm extends Component {
                 >
                     <div className={classes.drawerHeader} />
                     <DraggableColorList
-                        colors={this.state.colors}
+                        colors={colors}
                         removeColor={this.removeColor}
                         axis="xy"
-                        onSortEnd = {this.onSortEnd}
+                        onSortEnd={this.onSortEnd}
                     />
                 </main>
             </div>
