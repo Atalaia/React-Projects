84157594390a1d5216f0d656aaba8bbd4d210977
Maria Soage <maria.soage01@gmail.com>
2020-12-28 22:04:04 +0100

    Start style PaletteFormNav Component

--- colors-app/src/PaletteFormNav.js
+++ colors-app/src/PaletteFormNav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import classNames from 'classnames';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -9,6 +10,38 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from "@material-ui/core/Button";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+
+const drawerWidth = 400;
+
+const styles = theme => ({
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "64px"
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: 12,
+        marginLeft: 20
+    },
+    navBtns: {
+
+    }
+});
 
 class PaletteFormNav extends Component {
     constructor(props) {
@@ -18,7 +51,7 @@ class PaletteFormNav extends Component {
         };
         this.handleChange = this.handleChange.bind(this);
     }
-    
+
     componentDidMount() {
         ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
             this.props.palettes.every(
@@ -37,7 +70,7 @@ class PaletteFormNav extends Component {
         const { classes, open } = this.props;
         const { newPaletteName } = this.state;
         return (
-            <div>
+            <div className={classes.root}>
                 <CssBaseline />
                 <AppBar
                     position="fixed"
@@ -57,8 +90,10 @@ class PaletteFormNav extends Component {
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" color="inherit" noWrap>
-                            Persistent drawer
+                            Create a Palette
                         </Typography>
+                    </Toolbar>
+                    <div className={classes.navBtns}>
                         <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
                             <TextValidator
                                 label="Palette Name"
@@ -80,21 +115,21 @@ class PaletteFormNav extends Component {
                                 type="submit"
                             >
                                 Save Palette
-                            </Button>
-                            <Link to="/">
-                                <Button
-                                    variant="contained"
-                                    color="secondary"
-                                >
-                                    Go Back
                                 </Button>
-                            </Link>
                         </ValidatorForm>
-                    </Toolbar>
+                        <Link to="/">
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                            >
+                                Go Back
+                                </Button>
+                        </Link>
+                    </div>
                 </AppBar>
             </div>
         );
     }
 }
 
-export default PaletteFormNav;
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
